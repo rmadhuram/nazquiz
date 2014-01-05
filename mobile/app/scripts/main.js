14d6e711e2ab8d61cb439b4f8b9ee1ba2b7f161a
Raj <rmadhuram@yahoo.com>
2014-01-05 01:25:40 -0800

    initial experimentation on mobile client

---
+++ mobile/app/scripts/main.js
@@ -0,0 +1,11 @@
+
+(function() {
+    // to optimize performance
+    $.mobile.defaultPageTransition   = 'none'
+    $.mobile.defaultDialogTransition = 'none'
+    $.mobile.buttonMarkup.hoverDelay = 0;
+
+
+})();
+
+
